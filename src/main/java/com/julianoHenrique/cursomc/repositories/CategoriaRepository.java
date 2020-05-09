cab71f4823c5e1b4e93e3cb152dcfbfd6e04b186
Juliano Henrique <julianohenriquejhp@gmail.com>
2020-05-09 14:24:40 -0300

    Craido Repository e Service Para Categoria

---
+++ src/main/java/com/julianoHenrique/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,13 @@
+package com.julianoHenrique.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.julianoHenrique.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+	
+	
+
+}
